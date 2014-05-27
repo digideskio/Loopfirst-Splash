58979492509de4112c5522f0e32fb0964309cb3e
Lashan Dias <diaslashan@gmail.com>
2014-05-26 22:42:40 -0700

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+$('#mc-form').ajaxChimp({
+    url: 'http://loopfirst.us8.list-manage.com/subscribe/post?u=05bcd9785a35a2e97b6ffb520&amp;id=3133bd4d5d'
+});
+
+$('#signup').click(function() {
+    $('.wrap').addClass('slide');
+});
