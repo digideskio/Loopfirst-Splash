d2586bb50393ab10605f3236dbce361e31584864
Lashan Dias <diaslashan@gmail.com>
2014-05-26 23:57:26 -0700

    changed hero fonts, rewrote js, made bg fade on load

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,25 @@
 'use strict';
 
-$('#mc-form').ajaxChimp({
-    url: 'http://loopfirst.us8.list-manage.com/subscribe/post?u=05bcd9785a35a2e97b6ffb520&amp;id=3133bd4d5d'
-});
+function splashReady() {
+    $('#mc-form').ajaxChimp({
+        url: 'http://loopfirst.us8.list-manage.com/subscribe/post?u=05bcd9785a35a2e97b6ffb520&amp;id=3133bd4d5d'
+    });
 
-$('#signup').click(function() {
-    $('.wrap').addClass('slide');
-});
+    $('#signup').click(function() {
+        $('.wrap').addClass('slide');
+
+        setTimeout(function() {
+            $('#mc-email').focus();
+            $('#mc-email').prompt();
+        }, 500);
+    });
+}
+
+
+function splashLoad() {
+    $('body').addClass('loaded');
+}
+
+
+$(document).ready(splashReady);
+$(window).load(splashLoad);
